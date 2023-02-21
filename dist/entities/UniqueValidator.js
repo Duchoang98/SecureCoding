7da8672280bc73519fd98680b87b3401494aaabf
hoang <duchoang2612yh@gmail.com>
2023-02-21 05:44:24 +0100

    untrack files contained in the .gitignore file

--- dist/entities/UniqueValidator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, validate } from "class-validator";
-// @ValidatorConstraint({ async: true })
-// export class UniqueInColumnValidator implements ValidatorConstraintInterface {
-//     async validate(value: any, constraint: ValidationOptions) {
-//     }
-// }
